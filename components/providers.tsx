e5f422ea9c86ea83497a3f962d1e4f74143c6609
TI Motopema <ti@motopema.com.br>
2026-03-11 10:00:33 -0300

    feat: create hook useToast and update the application provider with the hook

--- components/providers.tsx
+++ components/providers.tsx
@@ -2,11 +2,13 @@
 
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "@/lib/queryClient";
+import { Toaster } from "@/components/ui/toaster";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
       {children}
+      <Toaster />
     </QueryClientProvider>
   );
 }
